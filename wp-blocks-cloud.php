3cc90e1b0ba127130eef8deb008ac12456fc50a2
Matthew Pritchett <matthew.r.pritchett@gmail.com>
2018-12-15 14:23:30 -0500

    Removing unnecessary code.

--- wp-blocks-cloud.php
+++ wp-blocks-cloud.php
@@ -38,29 +38,9 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'WPBLOCKSCLOUD', '1.0.0' );
 
 /**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-wp-blocks-cloud-activator.php
- */
-function activate_wp_blocks_cloud() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-blocks-cloud-activator.php';
-	Wp_Blocks_Cloud_Activator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-wp-blocks-cloud-deactivator.php
- */
-function deactivate_wp_blocks_cloud() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-blocks-cloud-deactivator.php';
-	Wp_Blocks_Cloud_Deactivator::deactivate();
-}
-
-// register_activation_hook( __FILE__, 'activate_wp_blocks_cloud' );
-// register_deactivation_hook( __FILE__, 'deactivate_wp_blocks_cloud' );
-
-/**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
 require plugin_dir_path( __FILE__ ) . 'includes/class-wp-blocks-cloud-assets.php';
+
 WP_Blocks_Cloud_Assets::init();
