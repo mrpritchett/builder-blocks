fb169524e163cc9aeb691a97697a44abfdaa6bee
Matthew Pritchett <matthew.r.pritchett@gmail.com>
2019-06-10 16:21:49 -0400

    Adds Hero to block includes.
    Removes Testimonials for now.

--- preprocess/scripts/blocks.js
+++ preprocess/scripts/blocks.js
@@ -1,9 +1,11 @@
 import RestaurantMenu from './blocks/RestaurantMenu/RestaurantMenu'
 import MenuItem from './blocks/MenuItem/MenuItem'
-import Testimonials from './blocks/Testimonials'
+// import Testimonials from './blocks/Testimonials'
+import Hero from './blocks/Hero/Hero'
 
 const { registerBlockType } = wp.blocks
 
 registerBlockType('builder-blocks/restaurantmenu', RestaurantMenu)
 registerBlockType('builder-blocks/menuitem', MenuItem)
-registerBlockType('builder-blocks/testimonials', Testimonials)
+// registerBlockType('builder-blocks/testimonials', Testimonials)
+registerBlockType('builder-blocks/hero', Hero)
